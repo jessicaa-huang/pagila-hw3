758a5f618a5633d53ff16cfd641de0171104aa56
jessicaa-huang <70783201+jessicaa-huang@users.noreply.github.com>
2026-03-23 00:31:01 -0700

    Updating 04.sql

--- sql/04.sql
+++ sql/04.sql
@@ -4,19 +4,26 @@
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
 
-SELECT DISTINCT actor.first_name, actor.last_name
+SELECT first_name, last_name 
 FROM actor
-INNER JOIN film_actor USING (actor_id)
-INNER JOIN film USING (film_id)
-INNER JOIN film_category USING (film_id)
-INNER JOIN category USING (category_id)
-WHERE category.name = 'Children'
-  AND NOT EXISTS (
+WHERE EXISTS (
+    SELECT
+    1
+    FROM
+    film_actor 
+    INNER JOIN film USING (film_id)
+    INNER JOIN film_category USING (film_id)
+    INNER JOIN category USING (category_id)
+    WHERE film_actor.actor_id = actor.actor_id
+   AND category.name = 'Children' 
+)
+AND NOT EXISTS (
     SELECT 1
-    FROM film_actor fa2
-    INNER JOIN film f2 USING (film_id)
-    INNER JOIN film_category fc2 USING (film_id)
-    INNER JOIN category c2 USING (category_id)
-    WHERE fa2.actor_id = actor.actor_id AND c2.name = 'Horror'
-  )
-ORDER BY actor.last_name;
+    FROM film_actor
+    INNER JOIN film USING (film_id)
+    INNER JOIN film_category USING (film_id)
+    INNER JOIN category USING (category_id)
+    WHERE film_actor.actor_id = actor.actor_id
+    AND category.name = 'Horror'
+)
+ORDER BY last_name ASC;
