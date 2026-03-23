44c2df80c6f395f8308978bae1ad57571591d359
jessicaa-huang <70783201+jessicaa-huang@users.noreply.github.com>
2026-03-23 01:00:47 -0700

    Updating 04.sql

--- sql/04.sql
+++ sql/04.sql
@@ -4,20 +4,22 @@
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
 
-SELECT DISTINCT actor.first_name, actor.last_name
+SELECT first_name, last_name
 FROM actor
-INNER JOIN film_actor USING (actor_id)
-INNER JOIN film USING (film_id)
-INNER JOIN film_category USING (film_id)
-INNER JOIN category USING (category_id)
-WHERE category.name = 'Children'
-  AND actor.actor_id NOT IN (
-      SELECT actor.actor_id
-      FROM actor
-      INNER JOIN film_actor USING (actor_id)
-      INNER JOIN film USING (film_id)
-      INNER JOIN film_category USING (film_id)
-      INNER JOIN category USING (category_id)
-      WHERE category.name = 'Horror'
-  )
-ORDER BY actor.last_name, actor.first_name;
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM film_actor
+    JOIN film USING (film_id)
+    JOIN film_category USING (film_id)
+    JOIN category USING (category_id)
+    WHERE name = 'Children'
+)
+AND actor_id NOT IN (
+    SELECT actor_id
+    FROM film_actor
+    JOIN film USING (film_id)
+    JOIN film_category USING (film_id)
+    JOIN category USING (category_id)
+    WHERE name = 'Horror'
+)
+ORDER BY last_name, first_name;
